d2e03e3f6603608ce983d450d457f2dc6e2a304b
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2020-11-30 14:53:37 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_compress_image
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_compress_image/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_compress_image
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
